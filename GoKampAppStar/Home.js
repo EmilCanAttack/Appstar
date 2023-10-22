783e7cc64ba89a4c3e03ff1451a2711a7e0311ae
EmilCanAttack <90251019+EmilCanAttack@users.noreply.github.com>
2023-10-22 03:07:23 +0200

    Ledige kampe implementeres
    Nu kan man se liste af hold som har oprettet sig gennem Reacts fantastiske FlatList tool

--- GoKampAppStar/Home.js
+++ GoKampAppStar/Home.js
@@ -1,46 +1,78 @@
-// Home.js
+// LedigeKampe.js
 import React from 'react';
-import { View, Text, Button, ImageBackground, StyleSheet } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { View, Text, Image, FlatList, StyleSheet } from 'react-native';
 
-const Home = ({ navigation }) => {
-  const handleLogout = async () => {
-    // Tilføj logik til at logge ud her, f.eks. rydning af AsyncStorage-data
+const data = [
+  {
+    id: 1,
+    logo: require('./assets/stadium.jpeg'), // Erstat med stien til holdets logo
+    klubnavn: "Taastrup Idrætsforening",
+    tomtekst: "",
+    dato: '2023-11-15',
+    division: 'Motionsrækken',
+    antalSpillere: '8 mand',
+    kontakt: "Emil Can Atan, (+45) 83 91 03 45",
+    adresse: "Gadehavegårdsvej 1, 2630 Taastrup"
+  },
+  {
+    id: 2,
+    logo: require('./assets/stadium.jpeg'), // Erstat med stien til holdets logo
+    dato: '15/11 19:00',
+    division: 'Division 4',
+    antalSpillere: '11 mand',
+    kontakt: "Lars Dahl, (+45) 23 56 43 12",
+    klubnavn: "Albertslund Idrætsforening",
+    tomtekst: "",
+    adresse: "Skallerne 14, 2620 Albertslund"
+  },
+  // Tilføj flere hold efter behov
+];
 
-    // Naviger tilbage til SignUp-skærmen (eller en anden skærm efter din logik)
-    navigation.navigate('Login');
-  };
-
+const LedigeKampe = () => {
   return (
-    <ImageBackground
-      source={require('./assets/stadium.jpeg')}
-      style={styles.backgroundImage}
-    >
-      <View style={styles.container}>
-        <Text style={styles.title}>Welcome to the Homepage</Text>
-        <Button title="Log Out" onPress={handleLogout} />
-        <Button title="Ledige Kampe" onPress={() => navigation.navigate('LedigeKampe')} />
-      </View>
-    </ImageBackground>
+    <View style={styles.container}>
+      <Text style={styles.title}>Ledige Kampe</Text>
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.itemContainer}>
+            <Image source={item.logo} style={styles.logo} />
+            <Text>{item.klubnavn}</Text>
+            <Text> {item.tomtekst}</Text>
+            <Text>Vil gerne spille: {item.dato}</Text>
+            <Text>Division: {item.division}</Text>
+            <Text>Antal mand: {item.antalSpillere}</Text>
+            <Text>Adresse: {item.adresse}</Text>
+            <Text>Kontakt: {item.kontakt}</Text>
+            
+          </View>
+        )}
+      />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  backgroundImage: {
-    flex: 1,
-    resizeMode: 'cover',
-  },
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    padding: 16,
   },
   title: {
     fontSize: 24,
-    textAlign: 'center',
-    color: 'white',
+    marginBottom: 10,
+  },
+  itemContainer: {
+    backgroundColor: 'white',
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 5,
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    resizeMode: 'contain',
   },
 });
 
-export default Home;
+export default LedigeKampe;
